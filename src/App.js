3207022045099019b557688b3d3e5e1926b418fe
Smiley!!2003:) <92297363+Smiley-2003@users.noreply.github.com>
2024-08-05 17:10:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// Filename - App.js
+
+import React, { useState } from "react";
+import Picker from "emoji-picker-react";  
+
+export default function Emoji() {
+    const [chosenEmoji, setChosenEmoji] = useState(null);
+
+    const onEmojiClick = (event, emojiObject) => {
+        setChosenEmoji(emojiObject);
+        console.log(emojiObject.target);
+    };
+
+    return (
+        <div>
+            <h3>Naina's Emoji Picker</h3>
+            {chosenEmoji ? (
+                <span>
+                    Your Emoji:
+                    <img
+                        style={{ width: "15px" }}
+                        src={chosenEmoji.target.src}
+                    />
+                </span>
+            ) : (
+                <span>No Emoji</span>
+            )}
+            <Picker onEmojiClick={onEmojiClick} />
+        </div>
+    );
+}
